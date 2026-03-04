8c56cbd4479f5d9509cdeec8e6ae520c55fae1c8
Don Barche <don.barche.de@gmail.com>
2026-03-04 17:23:36 +0100

    Adding the logic that dictates which status transitions are allowed in the enum itself.

--- src/main/java/com/expatica/todoservice/domain/TodoStatus.java
+++ src/main/java/com/expatica/todoservice/domain/TodoStatus.java
@@ -3,5 +3,13 @@ package com.expatica.todoservice.domain;
 public enum TodoStatus {
     NOT_DONE,
     DONE,
-    PAST_DUE
+    PAST_DUE;
+
+    public boolean canTransitionTo(TodoStatus next) {
+        return switch (this) {
+            case NOT_DONE -> next == DONE || next == PAST_DUE;
+            case DONE -> next == NOT_DONE;
+            case PAST_DUE -> false;
+        };
+    }
 }
